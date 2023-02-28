ac04eba398cc1719495067e8250c2e89a2450bbc
Professor Nobody <86370613+Professor-Nobody@users.noreply.github.com>
2023-02-28 12:59:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let typingText = document.querySelector('.change-txt')
+let Designation = ['Front End Developer','Back End Developer', 'Discord Manager'];
+
+let arrayIndex = 1;
+
+function ChangingTxt(){
+    if(arrayIndex<Designation.length){
+        typingText.innerHTML = Designation[arrayIndex];
+        arrayIndex = arrayIndex + 1;
+    }
+    else{
+        arrayIndex = 0;
+        typingText.innerHTML = Designation[arrayIndex];
+        arrayIndex = arrayIndex + 1;
+    }
+}
+ChangingTxt()
+setInterval(ChangingTxt,5000);
